7007899722a4f3f348ab2f0ded0c9ff801a4080a
SaltRibers <99395604+SaltRivers@users.noreply.github.com>
2022-04-23 15:01:34 +0800

    Create 快速模板.cpp

---
+++ 模板/快速模板.cpp
@@ -0,0 +1,12 @@
+inline int read()
+{
+    int x=0;
+    char c=getchar();
+    bool flag=0;
+    while(c<'0'||c>'9'){if(c=='-')flag=1;   c=getchar();}
+    while(c>='0'&&c<='9'){x=(x<<3)+(x<<1)+c-'0';c=getchar();}
+    return flag?-x:x;
+}
+————————————————
+版权声明：本文为CSDN博主「βartholomew」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
+原文链接：https://blog.csdn.net/qq_39809664/article/details/78408285
